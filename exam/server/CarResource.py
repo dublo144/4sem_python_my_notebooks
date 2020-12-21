5f0c75ed4c7d689af78ad50fa80237d19e0d60a5
Mads Brandt <mads.brandt93@gmail.com>
2020-12-21 09:56:37 +0100

    more exam

--- exam/server/CarResource.py
+++ exam/server/CarResource.py
@@ -8,7 +8,8 @@ import json
 from bson import ObjectId
 
 
-client = pymongo.MongoClient()
+client = pymongo.MongoClient(
+    "mongodb+srv://python:exam123@cluster0.wabpp.mongodb.net/cars?retryWrites=true&w=majority")
 db = client.python_exam
 collection = db.cars
 
@@ -50,19 +51,72 @@ def read_from_db():
 
 @app.route('/api/cars/get', methods=['GET'])
 def read_model_from_db():
-    model = request.args.get('model')
+    make = request.args.get('make')
     max_price = request.args.get('max_price')
-    res = collection.find({'Model': re.compile(
-        model, re.IGNORECASE), 'Pris': {'$lt': max_price}})
+    res = collection.find({'Make': re.compile(
+        make, re.IGNORECASE), 'Pris': {'$lt': max_price}})
     df = pd.DataFrame(list(res))
-    print(df)
-    print(model)
-    print(max_price)
     result = {}
     for index, row in df.iterrows():
         # result[index] = row.to_json()
         result[index] = dict(row)
     return jsonify(result)
+
+
+@app.route('/api/manufactorcount', methods=['GET'])
+def return_manufactor_count():
+    query = collection.aggregate([
+        {
+            "$match": {
+                "Make": {"$not": {"$size": 0}}
+            }
+        },
+        {"$unwind": "$Make"},
+        {
+            "$group": {
+                "_id": {"$toLower": "$Make"},
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$match": {
+                "count": {"$gte": 2}
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 100}
+    ])
+    df = pd.DataFrame(list(query))
+    result = to_json_response(df)
+    print(df)
+    return result
+
+
+@app.route('/api/pricemiles', methods=['GET'])
+def return_price_miles():
+    query = collection.find(
+        {}, {"Kilometer": 1, "Pris": 1, "_id": 0, }).limit(500)
+    df = pd.DataFrame(list(query))
+    result = to_json_response(df)
+    return result
+
+
+@app.route('/api/pricemiles/make', methods=['GET'])
+def return_price_miles_from_make():
+    manufactor = request.args.get('make')
+    res = collection.find({'Make': re.compile(
+        manufactor, re.IGNORECASE)}, {"Kilometer": 1, "Pris": 1, "_id": 0, }).limit(500)
+
+    df = pd.DataFrame(list(res))
+    result = to_json_response(df)
+    print(result)
+    return result
+
+
+@app.route('/api/dummy', methods=['GET'])
+def return_dummy_res():
+    print('hgey')
+    return jsonify({'Msg': 'HelloWorld'})
 
 
 if __name__ == '__main__':
